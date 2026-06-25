802f6fc5e0111f6f4e470e74ad484b9daf0b8bb9
Jayati Mahato <24jayatimahato@gmail.com>
2026-06-25 10:39:40 +0530

    changed the vite port to 5173.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
@@ -12,3 +13,6 @@ export default defineConfig({
     },
   },
 });
+
+
+    
